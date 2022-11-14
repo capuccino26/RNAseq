226f8369881bdc79fde80887ad967ccde5fd4ed9
Pedro Carvalho <pccrvl@gmail.com>
2022-11-14 16:22:06 -0300

    Update featurecounts.R

--- featurecounts.R
+++ featurecounts.R
@@ -3,7 +3,7 @@ aligned <- featureCounts(here::here("/starAligned/Aligned.sortedByCoordPICARD.SA
 ##Optional: Open directly from STAR
 aligned <- featureCounts(here::here("/starAligned/Aligned.sortedByCoord.bam"), isPairedEnd=TRUE)
 ##Optional: Using known genome
-aligned <- -a /rawData/genome.gtf -o /starAligned/Aligned.sortedByCoord.bam
+aligned <- featureCounts(-a /rawData/genome.gtf -o /starAligned/Aligned.sortedByCoord.bam)
 
 #Save and format Table as CSV
 write.table(x=data.frame(aligned$annotation[,c("GeneID","Length")],aligned$counts,stringsAsFactors=False),file="/starAligned/counts.csv",quote=FALSE,row.names=FALSE)
